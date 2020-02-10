0c0de3d67bab2067a3c342332796bcd6c02ca671
zhanzi123 <1025042582@qq.com>
2020-02-10 21:52:02 +0800

    rest framework
    restframework尝试

---
+++ web_app/django_rest/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User,Group
+from rest_framework import viewsets
+from api.serializers import UserSerializer,GroupSerializer
+# Create your views here.
+#ViewSets定义视图的展现方式
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
